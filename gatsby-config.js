020d6fe85b2407505d08147bfc9481efe7aac2c5
jyajoo <jyajoo1020@gmail.com>
2022-06-28 14:22:32 +0900

    add: image 플러그인 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     title: `My Blog`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }

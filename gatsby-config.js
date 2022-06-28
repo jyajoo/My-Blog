4d4c37d2c0a1fc48750300b7fe19b0c694a365d5
jyajoo <jyajoo1020@gmail.com>
2022-06-28 15:13:46 +0900

    add: GraphQL data 이용해보기

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `My Blog`,
-    siteUrl: `https://www.yourdomain.tld`,
+    description: "Development Blog",
   },
   plugins: [
     "gatsby-plugin-image",

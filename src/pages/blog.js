4413591db74bc52bfe9b472cb1ce2cb3b4b6b18e
jyajoo <jyajoo1020@gmail.com>
2022-06-28 16:18:23 +0900

    chore: file list

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,28 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { graphql, useStaticQuery } from "gatsby";
 
 function BlogPage() {
+
+  const data = useStaticQuery(graphql`
+    query {
+      allFile {
+        nodes {
+          name
+        }
+      }
+    }
+  `);
+
+
   return (
     <Layout pageTitle="My Blog Posts">
       <p>My posts will go in here</p>
+      <ul>
+        {data.allFile.nodes.map((node) => (
+          <li key={node.name}>{node.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 }
